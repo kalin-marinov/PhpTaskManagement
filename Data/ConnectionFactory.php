0fdf8a44433ae9942a434b4daa70e15b41eb93e7
kalin-marinov <kalimar94@gmal.com>
2017-02-05 13:15:43 +0200

    changing connecton to work with azure db

--- Data/ConnectionFactory.php
+++ Data/ConnectionFactory.php
@@ -4,10 +4,10 @@ class ConnectionFactory{
     
     public static function create() : PDO {
         // Default parameters:
-        $host = "localhost";
+        $host = "eu-cdbr-azure-west-d.cloudapp.net";
         $db = "taskmanagement";
-        $user = "testUser";
-        $pass = "testPassword";
+        $user = "b42e25c9b39192";
+        $pass = "d7cd0911";
         
         $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
         return $conn;

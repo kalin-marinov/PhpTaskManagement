9e40d4e28b412dcb64e16e333a767a3e31f5d173
kalin-marinov <kalimar94@gmal.com>
2017-02-11 00:45:44 +0200

    creating basic connection pooling

--- Factories/ConnectionFactory.php
+++ Factories/ConnectionFactory.php
@@ -2,15 +2,23 @@
 
 class ConnectionFactory{
     
+    static $conn;
+    
     public static function create() : PDO {
         // Default parameters:
         $host = "eu-cdbr-azure-west-d.cloudapp.net";
         $db = "taskmanagement";
         $user = "b42e25c9b39192";
         $pass = "d7cd0911";
-
-    
-        $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+        
+        // $host = "localhost:3306";
+        // $db = "taskmanagement";
+        // $user = "testUser";
+        // $pass = "testPassword";
+        
+        if(!isset($conn))
+          $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+         
         return $conn;
     }
 }
